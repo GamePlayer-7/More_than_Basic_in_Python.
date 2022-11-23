5d6b9337b141eec5c742bbbaeba589619cbc7e0e
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2022-11-23 10:57:45 +0530

    Create Tuple_Unpacking.py

---
+++ Split & Tuple Unpacking/Tuple_Unpacking.py
@@ -0,0 +1,15 @@
+a = ("Mangoes", 5000, "Devgad")
+
+(fruit, price, location) = a
+
+print(fruit)             # print fruit name
+
+print(price)             # print price
+
+print(location)          # print location
+
+# Below is the Output :
+
+Mangoes
+5000
+Devgad
